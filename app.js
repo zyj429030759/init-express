4ca8abd8d69b34828223532c336a0a958634f934
zhangyajing <zhangyj@yxt.com>
2022-11-18 11:51:06 +0800

    feat: 项目初始化

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const cors = require('cors')
+
+/// 创建服务器实例对象
 const app = express()
 
+// 配置cors中间件，解决跨域问题
+app.use(cors())
+// 配置解析表单数据的中间件(application/x-www-form-urlencoded格式)，否则req.body拿不到数据
+app.use(express.urlencoded({ extended: false }))
+// 配置解析表单数据为application/json 格式的内置中间件
+// app.use(express.json())
+
+
+
+
+// 启动服务器
 app.listen(3002, () => {
     console.log('app running at http://127.0.0.1:3002')
 })
